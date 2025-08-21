7c3f3f623e626c87713c4266909e512d3f490f96
Cosmin Pirciu <cosmin.git@adixor.nl>
2025-08-21 19:42:40 +0200

    Cleans setup-mess afterwards

--- setup.sh
+++ setup.sh
@@ -3,11 +3,14 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install python3 python3-serial python3-mido git -y
 sudo systemctl disable hciuart
-mkdir build
-cd build
+mkdir floppio
+cd floppio
 git clone https://github.com/TheLittleBits/FloppIO.git
 mv FloppIO/*.py ./
 mkdir midi
 mv FloppIO/midi/*.mid midi/
+mv FloppIO/pico/floppy/floppy.elf ./
+mv FloppIO/pico/scanner/scanner.elf ./
+rm FloppIO/ -rf
 sudo nano /boot/firmware/config.txt
 sudo reboot
