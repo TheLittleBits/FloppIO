df97bfeae23526e97343b41cacc039cd4d0c366a
Cosmin Pirciu <cosmin.git@adixor.nl>
2025-09-06 17:18:34 +0200

    Doesn't depend on origin

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 sudo raspi-config
 sudo apt update
 sudo apt upgrade -y
-sudo apt install python3 python3-serial python3-mido git -y
+sudo apt install python3 python3-serial python3-mido -y
 sudo systemctl disable hciuart
 mkdir floppio
-cd floppio
-git clone https://github.com/TheLittleBits/FloppIO.git
-mv FloppIO/*.py ./
+mv * floppio/
+cp floppio/*.py ./
 mkdir midi
-mv FloppIO/example-midi/*.mid midi/
-mv FloppIO/pico/floppy/floppy.elf ./
-mv FloppIO/pico/scanner/scanner.elf ./
-rm FloppIO/ -rf
+cp floppio/example-midi/*.mid midi/
+cp floppio/pico/floppy/floppy.elf ./
+cp floppio/pico/scanner/scanner.elf ./
 sudo nano /boot/firmware/config.txt
 sudo reboot
